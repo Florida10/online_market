31e5f03dd7428c7436304bb3938d31d31c7e01d6
Florida10 <floridaxhani@gmail.com>
2018-10-27 15:47:05 +0200

    27.10.2018 Marjos

--- app/User.php
+++ app/User.php
@@ -15,7 +15,8 @@ class User extends Authenticatable
      *
      * @var array
      */
-    protected $fillable = ['email', 'password',
+    protected $fillable = [
+        'name', 'email', 'password',
     ];
 
     /**
