4674d4d3ccd5322645cbceff6f5c1329db211ca1
Florida10 <floridaxhani@gmail.com>
2018-11-25 23:44:56 +0200

    Marjos commit

--- app/Http/Controllers/Userscontroller.php
+++ app/Http/Controllers/Userscontroller.php
@@ -55,7 +55,7 @@ class Userscontroller extends Controller
 
                 }
             }
-
+             //checking 
             //checking if user_roli has value
 
             if(empty($user_roli)){
